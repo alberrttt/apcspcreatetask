8dc8a8963fddb366b5b9906ab16d52b332117ce8
Your Name <you@example.com>
2023-03-28 08:31:19 -0700

    add trash can

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -102,14 +102,35 @@
 	<!-- there are no tasks to display, so show an empty div -->
 	<div />
 {:else}
-	<div class="bg-slate-900 text-white mb-2 p-2 rounded-md max-w-fit pr-8">
+	<div class="bg-slate-900 text-white mb-2 p-2 rounded-md max-w-fit">
 		<ul>
 			<!-- this statement calls the procedure and iterates through the return value, creating a UI element for each entry -->
-			{#each tasks_in_order_and_age(tasks, filter_by) || [] as task}
+			{#each tasks_in_order_and_age(tasks, filter_by) || [] as task, i}
 				<li class="list-disc">
 					<div class="flex flex-row my-1 items-center">
-						<p class="mr-1 text-lg font-semibold">{task.name}</p>
-						<p class="text-gray-500">{priority_to_string(task.priority)}</p>
+						<div class="flex flex-row items-center">
+							<p class="mr-1 text-lg font-semibold">{task.name}</p>
+							<p class="text-gray-500">{priority_to_string(task.priority)}</p>
+						</div>
+						<div
+							class="cursor-pointer"
+							on:click={() => {
+								tasks.splice(i, 1);
+								tasks = tasks;
+							}}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								fill="red"
+								viewBox="0 0 24 24"
+								class="max-w-min ml-4"
+								><path
+									d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"
+								/></svg
+							>
+						</div>
 					</div>
 				</li>
 			{/each}
