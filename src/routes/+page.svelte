7be823886925189c671c68a3129d5c52d6a5d661
Your Name <you@example.com>
2023-03-29 08:40:47 -0700

    make the svg its own component, and rename functions to make it clearer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import TrashcanIcon from "./trashcanicon.svelte";
+
 	interface Task {
 		name: string;
 		priority: Priority;
@@ -34,10 +36,10 @@
 	}
 
 	let tasks = new Array<Task>();
-	function tasks_in_order_and_age(
+	function filter_tasks(
 		tasks: Task[],
 		priorities: Priority[] | undefined
-	): Task[] | undefined {
+	): Task[] {
 		if (priorities != undefined) {
 			let filtered = new Array<Task>();
 			for (const task of tasks) {
@@ -51,7 +53,7 @@
 			}
 			return filtered;
 		} else {
-			return undefined;
+			return [];
 		}
 	}
 	let filter_by: Priority[] | undefined = [
@@ -59,7 +61,7 @@
 		Priority.Medium,
 		Priority.High,
 	];
-	$: len_of_sorted_tasks = (tasks_in_order_and_age(tasks, filter_by) || [])
+	$: len_of_sorted_tasks = (filter_tasks(tasks, filter_by) || [])
 		.length;
 	let priority: string = "Low";
 	let name: string = "";
@@ -95,8 +97,9 @@
 			// reset the inputs
 			name = "";
 			priority = "Low";
-		}}>Add</button
-	>
+		}}
+		>Add
+	</button>
 </div>
 {#if tasks.length == 0 || len_of_sorted_tasks == 0}
 	<!-- there are no tasks to display, so show an empty div -->
@@ -105,14 +108,14 @@
 	<div class="bg-slate-900 text-white mb-2 p-2 rounded-md max-w-fit">
 		<ul>
 			<!-- this statement calls the procedure and iterates through the return value, creating a UI element for each entry -->
-			{#each tasks_in_order_and_age(tasks, filter_by) || [] as task, i}
+			{#each filter_tasks(tasks, filter_by) as task, i}
 				<li class="list-disc">
 					<div class="flex flex-row my-1 items-center">
 						<div class="flex flex-row items-center">
 							<p class="mr-1 text-lg font-semibold">{task.name}</p>
 							<p class="text-gray-500">{priority_to_string(task.priority)}</p>
 						</div>
-						<div
+						<button
 							class="cursor-pointer"
 							on:click={() => {
 								tasks.splice(i, 1);
@@ -120,18 +123,8 @@
 								tasks = tasks;
 							}}
 						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								fill="red"
-								viewBox="0 0 24 24"
-								class="max-w-min ml-4"
-								><path
-									d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"
-								/></svg
-							>
-						</div>
+							<TrashcanIcon />
+						</button>
 					</div>
 				</li>
 			{/each}
